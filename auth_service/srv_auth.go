8344b67595d602ea597407ed330ffe195bb1092c
eco9999 <emrecanocak@windowslive.com>
2020-03-29 21:56:16 +0300

    error ->  errorx imp.

--- auth_service/srv_auth.go
+++ auth_service/srv_auth.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"database/sql"
-	"errors"
+	"errorx"
 	"io/ioutil"
 	"jin"
 	"log"
@@ -37,18 +37,18 @@ var (
 	responseScheme *jin.Scheme
 
 	// errors
-	authFail       error = errors.New("Wrong password.")
-	recordNotExist error = errors.New("Record Not Exists.")
-	missingEnvFile error = errors.New("Missing environment file or wrong file directory.")
+	missingEnvFile *errorx.Error = errorx.New("Fatal Error", "Missing environment file or wrong file directory", 2)
+	authFail       *errorx.Error = errorx.New("Database", "Wrong password", 0)
+	recordNotExist *errorx.Error = errorx.New("Database", "Record Not Exists", 1)
+	statError      *errorx.Error = errorx.New("Service", "Status method not allowed", 3)
+	authFailed     *errorx.Error = errorx.New("Service", "Authentication Request Failed", 4)
+	authGranted    *errorx.Error = errorx.New("Service", "Authentication Request Granted", 5)
 
 	// log strings
-	srvStart    string = ">> Authentication Service Started."
-	srvEnd      string = ">> Authentication Service Shutdown Unexpectedly. Error:"
-	reqArrived  string = ">> Request Arrived At"
-	reqBody     string = ">> Request Body:"
-	statError   string = ">> Status method not allowed"
-	authFailed  string = ">> Authentication Request Failed:"
-	authGranted string = ">> Authentication Request Granted."
+	srvStart   string = ">> Authentication Service Started."
+	srvEnd     string = ">> Authentication Service Shutdown Unexpectedly. Error:"
+	reqArrived string = ">> Request Arrived At"
+	reqBody    string = ">> Request Body:"
 )
 
 func init() {
@@ -101,7 +101,7 @@ func authHandle(w http.ResponseWriter, r *http.Request) {
 	// body read for json parse.
 	json, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		failHandle(w, authFailed+err.Error(), http.StatusInternalServerError)
+		failHandle(w, err, http.StatusInternalServerError)
 		return
 	}
 	defer r.Body.Close()
@@ -109,9 +109,9 @@ func authHandle(w http.ResponseWriter, r *http.Request) {
 	log.Println(reqBody, string(json))
 
 	// record check core function.
-	key, err := checkRecord(envServiceMap["userTable"], json)
+	key, status, err := checkRecord(envServiceMap["userTable"], json)
 	if err != nil {
-		failHandle(w, err.Error(), http.StatusUnauthorized)
+		failHandle(w, err, status)
 		return
 	}
 	doneHandle(w, key)
@@ -125,50 +125,48 @@ func dbConn() (*sql.DB, error) {
 	return db, nil
 }
 
-func checkRecord(table string, json []byte) (string, error) {
+func checkRecord(table string, json []byte) (string, int, error) {
 	db, err := dbConn()
 	if err != nil {
-		return "", err
+		return "", http.StatusInternalServerError, err
 	}
 	defer db.Close()
 
 	jsonMap, err := jin.GetMap(json)
 	if err != nil {
-		return "", err
+		return "", http.StatusInternalServerError, err
 	}
 	primaryKey := envServiceMap["primKey"]
 	query := seecool.Select(table).Equal(primaryKey, jsonMap[primaryKey])
 	result, err := seecool.QueryJson(db, query)
 	if err != nil {
-		return "", err
+		return "", http.StatusInternalServerError, err
 	}
-
 	if string(result) == "[]" {
-		return "", recordNotExist
+		return "", http.StatusBadRequest, recordNotExist
 	}
-
 	resultMap, err := jin.GetMap(result, "0")
 	if err != nil {
-		return "", err
+		return "", http.StatusInternalServerError, err
 	}
 	passwordKey := envServiceMap["passKey"]
 	if resultMap[passwordKey] != jsonMap[passwordKey] {
-		return "", authFail
+		return "", http.StatusUnauthorized, authFail
 	}
 
-	return resultMap[envServiceMap["returnKey"]], nil
+	return resultMap[envServiceMap["returnKey"]], http.StatusInternalServerError, nil
 }
 
-func statusFailed(err string) []byte {
-	return responseScheme.MakeJson("Failed", "null", seecool.EscapeQuote(err))
+func statusFailed(err error) []byte {
+	return responseScheme.MakeJson("Failed", "null", seecool.EscapeQuote(err.Error()))
 }
 
 func statusGranted(id string) []byte {
 	return responseScheme.MakeJson("OK", id, "null")
 }
 
-func failHandle(w http.ResponseWriter, err string, status int) {
-	log.Println(authFailed, err)
+func failHandle(w http.ResponseWriter, err error, status int) {
+	log.Println(authFailed.Link(err))
 	w.WriteHeader(status)
 	w.Write(statusFailed(err))
 }
